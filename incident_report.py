e4b2e447e074e4c0b032e22e951b920e0bf1a26b
ali haris khan <ali.50753@iqra.edu.pk>
2023-12-11 12:41:04 +0500

    commit

--- incident_report.py
+++ incident_report.py
@@ -98,6 +98,7 @@ class Incident_Report(db.Model):
                 )
                 print("condition2")
 
+
                 return render_template("searched_incident_report.html", results=results)
 
             elif all(condition3):
