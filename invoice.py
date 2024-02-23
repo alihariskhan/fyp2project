d7139f531dcc50bbcdfcc68ac61be4cfa0c9a13a
HaNi <ali.50753@iqra.edu.pk>
2024-02-23 12:00:49 +0500

    commit

--- invoice.py
+++ invoice.py
@@ -1,18 +1,31 @@
 from datetime import datetime
 
+from flask import render_template
+from flask_login import current_user
+from sqlalchemy import func
+
+from client_guard_reservation import Client_Guard_Reservation
+from guard_reservation import Guard_reservation
+from sql import db
+
+from client import Client
+
 
 class Invoice:
     def invoice(self):
-        start_time_str = '5:30:00'  # Example time
-        end_time_str = '15:45:30'  # Example time
+        client = Client.query.filter(Client.client_id == current_user.client_id).first()
+        date = datetime.now().date()
 
-        start_time = datetime.strptime(start_time_str, '%H:%M:%S')
-        end_time = datetime.strptime(end_time_str, '%H:%M:%S')
-
-        result = (end_time - start_time).total_seconds()/3600
-
-        result_rounded = round(result, 2)
-
-        print(f"{result_rounded}")
-
-        return '123'
+        reservation_details = (
+            db.session.query(
+                Guard_reservation.reservation_id,
+                func.count(Client_Guard_Reservation.guard_id).label('guard_count')
+            )
+            .join(Client_Guard_Reservation,
+                  Guard_reservation.reservation_id == Client_Guard_Reservation.reservation_id)
+            .filter(Client_Guard_Reservation.client_id == current_user.client_id)
+            .group_by(Guard_reservation.reservation_id)
+            .all()
+        )
+        print(f"{reservation_details}")
+        return render_template("invoice.html", client=client, date=date, reserv=reservation_details)
